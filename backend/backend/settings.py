593ab602b47e050b98a0eb4ed56d079ff3ec5a7a
Михаил Матросов <matrosovmn@gmail.com>
2023-07-16 18:41:29 +0500

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['130.193.53.215', '127.0.0.1', '8000', 'zelemastudy.hopto.org']
 
 
 # Application definition
